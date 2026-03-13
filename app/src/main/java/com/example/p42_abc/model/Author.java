ff2bab233b8b678fb8cd9c6aab3b5df0d70fa5ce
Omar <tonemail@example.com>
2026-03-13 19:57:33 +0300

    creation de la classe model et viewModel

---
+++ app/src/main/java/com/example/p42_abc/model/Author.java
@@ -0,0 +1,32 @@
+package com.example.p42_abc.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Author {
+    private int id;
+    private String name; // Tu pourras séparer prénom/nom plus tard si l'API l'exige
+
+    public Author(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Getters
+    public int getId() { return id; }
+    public String getName() { return name; }
+
+    // Setters
+    public void setId(int id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+
+    // Méthode temporaire pour générer de faux auteurs pour tester l'interface
+    public static List<Author> getFakeAuthors() {
+        List<Author> list = new ArrayList<>();
+        list.add(new Author(1, "Victor Hugo"));
+        list.add(new Author(2, "J.K. Rowling"));
+        list.add(new Author(3, "George Orwell"));
+        list.add(new Author(4, "Agatha Christie"));
+        return list;
+    }
+}
