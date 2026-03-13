82e6905d3431148f671c6aff33a6d89f3d2294b1
Cl1ient <teekzie68@gmail.com>
2026-03-13 22:31:09 +0100

    Ajout de la liste de livres dans le MutableLiveData du BookViewModel

--- app/src/main/java/com/example/p42_abc/viewmodels/BookViewModel.java
+++ app/src/main/java/com/example/p42_abc/viewmodels/BookViewModel.java
@@ -1,6 +1,28 @@
 package com.example.p42_abc.viewmodels;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.p42_abc.models.Book;
+
+import java.util.List;
+
 public class BookViewModel extends ViewModel {
+
+    // ! list de book
+    private final MutableLiveData<List<Book>> booksData = new MutableLiveData<>();
+
+    public BookViewModel(){
+        loadBooks();
+    }
+
+    public LiveData<List<Book>> getBooks(){
+        return booksData;
+    }
+
+    private void loadBooks(){
+        List<Book> list = Book.getBooks();
+        booksData.setValue(list);
+    }
 }
