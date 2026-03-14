dc387cbab640daede5708095b3dcd5a10c3fade2
Cl1ient <teekzie68@gmail.com>
2026-03-14 17:49:55 +0100

    Implémentation des méthodes de BookAdapter et BookViewHolder

--- app/src/main/java/com/example/p42_abc/adapters/BookAdapter.java
+++ app/src/main/java/com/example/p42_abc/adapters/BookAdapter.java
@@ -1,10 +1,13 @@
 package com.example.p42_abc.adapters;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.p42_abc.R;
 import com.example.p42_abc.models.Book;
 
 import java.util.ArrayList;
@@ -17,17 +20,19 @@ public class BookAdapter extends RecyclerView.Adapter<BookViewHolder> {
     @NonNull
     @Override
     public BookViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_book, parent, false);
+        return new BookViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull BookViewHolder holder, int position) {
-
+        Book book = _bookList.get(position);
+        holder.bind(book);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return _bookList != null ? _bookList.size() : 0;
     }
 
     public void setBooks(List<Book> books){
--- app/src/main/java/com/example/p42_abc/adapters/BookViewHolder.java
+++ app/src/main/java/com/example/p42_abc/adapters/BookViewHolder.java
@@ -1,12 +1,23 @@
 package com.example.p42_abc.adapters;
 
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.p42_abc.models.Book;
+
 public class BookViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView titleTextView;
+    private TextView descriptionTextView;
     public BookViewHolder(@NonNull View itemView) {
         super(itemView);
+    }
+
+    public void bind(Book book){
+        titleTextView.setText(book.getTitle());
+        descriptionTextView.setText(book.getDescription());
     }
 }
