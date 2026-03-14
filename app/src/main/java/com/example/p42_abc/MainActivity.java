b9ec766d0cc6c7f4ea157d0176fc590aa20c6017
Cl1ient <teekzie68@gmail.com>
2026-03-14 17:56:29 +0100

    Ajout de BookListFragment à MainActivity

--- app/src/main/java/com/example/p42_abc/MainActivity.java
+++ app/src/main/java/com/example/p42_abc/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.p42_abc.views.BookListFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +22,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.main, new BookListFragment()).commit();
+        }
     }
 }
