79a22b5bd1939e4a9a71dfc1a02ca2eb6f612a5a
Cl1ient <teekzie68@gmail.com>
2026-03-14 18:01:01 +0100

    Fix crash

--- app/src/main/java/com/example/p42_abc/adapters/BookViewHolder.java
+++ app/src/main/java/com/example/p42_abc/adapters/BookViewHolder.java
@@ -6,18 +6,24 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.p42_abc.R;
 import com.example.p42_abc.models.Book;
 
 public class BookViewHolder extends RecyclerView.ViewHolder {
 
-    private TextView titleTextView;
-    private TextView descriptionTextView;
+    private final TextView titleTextView;
+    private final TextView descriptionTextView;
+
     public BookViewHolder(@NonNull View itemView) {
         super(itemView);
+        titleTextView = itemView.findViewById(R.id.text_view_book_title);
+        descriptionTextView = itemView.findViewById(R.id.text_view_book_description);
     }
 
     public void bind(Book book){
-        titleTextView.setText(book.getTitle());
-        descriptionTextView.setText(book.getDescription());
+        if(book != null) {
+            titleTextView.setText(book.getTitle());
+            descriptionTextView.setText(book.getDescription());
+        }
     }
 }
